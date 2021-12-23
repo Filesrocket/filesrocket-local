8319abba4bf6a413bc57a54c876a3b2fba6a80b0
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-12-22 20:38:20 -0600

    Images and create file test was added

--- test/services/file.service.spec.ts
+++ test/services/file.service.spec.ts
@@ -1,3 +1,42 @@
-test("2 + 2 equal to 4", () => {
-  expect(2 + 2).toBe(4);
+import { createReadStream } from "fs";
+import { resolve } from "path";
+jest.mock("filesrocket");
+
+import { LocalFileService } from "../../src/index";
+
+const localService = new LocalFileService({
+  pagination: { default: 15, max: 50 },
+  directory: "uploads",
+  host: "http://localhost:3030"
+});
+
+export const FILENAMES = [
+  "one.png",
+  "two.png",
+  "three.png",
+  "four.png",
+  "five.png"
+];
+
+beforeEach(() => {
+  jest.setTimeout(60 * 5 * 1000);
+});
+
+describe("Create files.", () => {
+  test("Create many file", async () => {
+    const results = await Promise.all(
+      FILENAMES.map((name) => {
+        const path: string = resolve(`test/fixtures/${name}`);
+        return localService.create({
+          name,
+          stream: createReadStream(path),
+          fieldname: "files",
+          encoding: "",
+          mimetype: ""
+        });
+      })
+    );
+
+    expect(results.length).toBe(FILENAMES.length);
+  });
 });

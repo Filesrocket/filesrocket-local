b433529022d562b44dbef00c78b4f9ad1024b935
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-01-01 12:12:08 -0600

    Updated builder entity

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -13,22 +13,25 @@ export class BaseService {
   async builder(path: string): Promise<ResultEntity> {
     const fullpath: string = resolve(path);
     const { directory: folder, host } = this.options;
-    const { base: filename, ext, dir } = parse(fullpath);
+    const { base: name, ext, dir } = parse(fullpath);
 
     const regex = new RegExp(`${ folder }.+`, "g");
     const [directory] = dir.match(regex) || [folder];
     const chunks: string[] = directory.split(sep);
 
     const stat = await statAsync(fullpath);
-    const url: string = `${ host }/${ chunks.join("/") }/${ filename }`;
+    const url: string = `${ host }/${ chunks.join("/") }/${ name }`;
+
+    const items: string[] = chunks.slice(1, chunks.length);
+    items.push(name);
 
     return {
       id: url,
-      name: filename,
+      name,
       ext,
       url,
       size: stat.size,
-      dir: chunks.join("/"),
+      dir: items.join("/"),
       createdAt: stat.birthtime,
       updatedAt: stat.atime
     };

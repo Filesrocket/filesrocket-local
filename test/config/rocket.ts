df1a7d6acf8c492c2b30d6b90e4c81c7b9219fca
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-01-28 17:19:31 -0600

    Deeleted unnecessary packages

--- test/config/rocket.ts
+++ test/config/rocket.ts
@@ -2,15 +2,15 @@ import {
   LocalDirectoryService,
   LocalFileService,
   LocalOptions
-} from "../../src/index";
+} from '../../src/index'
 
 const options: LocalOptions = {
   pagination: { default: 15, max: 50 },
-  directory: "uploads",
-  host: "http://localhost:3030"
+  directory: 'uploads',
+  host: 'http://localhost:3030'
 }
 
 // File Service.
-export const File = new LocalFileService(options);
+export const File = new LocalFileService(options)
 // Directory Service.
-export const Directory = new LocalDirectoryService(options);
+export const Directory = new LocalDirectoryService(options)
